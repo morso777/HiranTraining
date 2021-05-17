40ea5b536f77d13a10d1ff5f6ecc0bc04daa3607
Hiran Pena <hiran.penatoledo@finning.com>
2021-05-17 11:09:19 -0500

    Adding DeleteCookie

---
+++ src/test/java/com/hiran/training/DeleteCookie.java
@@ -0,0 +1,48 @@
+package com.hiran.training;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DeleteCookie {
+	
+	 @Test
+		public static void main(String[] args)throws InterruptedException {	
+		 
+			 		 	
+			 String urlIEDEV= "https://www.google.com";		 
+			 						
+		     WebDriver driver;
+			 System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");	
+			 driver = new ChromeDriver ();
+			
+			 
+			 //open url
+			 driver.get(urlIEDEV);
+			 driver.manage().window().maximize();
+			 driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS) ;
+			 Set<org.openqa.selenium.Cookie> allCookies= driver.manage().getCookies();
+			 
+			 //Set<Cookie> allCookies= driver.manage().getCookies();
+			 System.out.println("Count ofcookies = " + allCookies.size());
+			 
+			 for (org.openqa.selenium.Cookie cookie: allCookies) {
+				 System.out.println ("Name   = "+ cookie.getName());
+				 System.out.println ("Domain = "+ cookie.getDomain());
+				 System.out.println ("Path   = "+ cookie.getPath());
+				 System.out.println ("Value  = "+ cookie.getValue());
+				 System.out.println ("====================================");
+				 
+				 driver.manage().deleteAllCookies();
+				 
+				 Set<org.openqa.selenium.Cookie> afterDel= driver.manage().getCookies();
+				 System.out.println("Count of cookies after delete is = " + afterDel.size());
+				 
+				 
+			 }
+
+	 }
+}
